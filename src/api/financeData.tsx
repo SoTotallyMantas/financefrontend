1ce829871ec2891b09986648c653e8574fa343e8
Mantas Trojanovskis <mantastrojak@gmail.com>
2025-04-18 02:35:41 +0300

    ta

--- src/api/financeData.tsx
+++ src/api/financeData.tsx
@@ -57,14 +57,15 @@ export interface NewsArticle {
     pc: number;
   }
 export async function fetchStockPrice(symbol:string): Promise<StockPriceData>{
- const response = await fetch(`${process.env.APIURL}/finance/StockPrice?symbol=${symbol}`)
+  const apiUrl = process.env.APIURL;
+ const response = await fetch(`${apiUrl}/finance/StockPrice?symbol=${symbol}`)
  const data: StockPriceData = await response.json() as StockPriceData
  return data;
 };
   // Retrieve Stock Symbols
 export async function fetchSymbols(): Promise<SymbolData[]>{
-   
-    const response = await fetch('${process.env.APIURL}/finance/Symbols')
+   const apiUrl = process.env.APIURL;
+    const response = await fetch(`${apiUrl}/finance/Symbols`)
   const data: SymbolData[] = await response.json() as SymbolData[];
   const formatedData: SymbolData[] = data.map(symbol => ({
     ...symbol,
@@ -75,32 +76,32 @@ export async function fetchSymbols(): Promise<SymbolData[]>{
   };
   // Get Market News
   export async function  fetchMarketNews(): Promise<NewsArticle[]>{
-   
-    const response = await fetch('${process.env.APIURL}/finance/MarketNews')
+   const apiUrl = process.env.APIURL;
+    const response = await fetch(`${apiUrl}/finance/MarketNews`)
   const data: NewsArticle[] = await response.json() as NewsArticle[];
   return data;
   console.log(data);
   };
   // get Company news of stock
   export async function  fetchCompanyNews(symbol: string): Promise<NewsArticle[]>{
-   
-    const response = await fetch(`${process.env.APIURL}/finance/CompanyNews?symbol=${symbol}`)
+   const apiUrl = process.env.APIURL;
+    const response = await fetch(`${apiUrl}/finance/CompanyNews?symbol=${symbol}`)
     const data: NewsArticle[] = await response.json() as NewsArticle[];
     return data;
   console.log(data);
   };
   // Get Stock metrics of Stock
   export async function  fetchStockMetric(symbol:string): Promise<StockMetricData>{
-   
-    const response = await fetch(`${process.env.APIURL}/finance/StockMetric?symbol=${symbol}`)
+   const apiUrl = process.env.APIURL;
+    const response = await fetch(`${apiUrl}/finance/StockMetric?symbol=${symbol}`)
     const data: StockMetricData = await response.json() as StockMetricData;
     return data;
   console.log(data);
   };
   // TODO 
   export async function GetFavorite(token:string): Promise<FavoritedData[]>{
-   
-    const response = await fetch('${process.env.APIURL}/user/favorites', {
+   const apiUrl = process.env.APIURL;
+    const response = await fetch(`${apiUrl}/user/favorites`, {
       method: 'GET',
       headers: {
         Authorization: `Bearer ${token}`
@@ -114,8 +115,8 @@ export async function fetchSymbols(): Promise<SymbolData[]>{
   console.log(data);
   };
   export async function PostFavorite(token:string,symbol:string){
-   
-    const response = await fetch(`${process.env.APIURL}/user/favorites?symbol=${symbol}`, {
+      const apiUrl = process.env.APIURL;
+    const response = await fetch(`${apiUrl}/user/favorites?symbol=${symbol}`, {
       method: 'POST',
       headers: {
         Authorization: `Bearer ${token}`
@@ -130,8 +131,8 @@ export async function fetchSymbols(): Promise<SymbolData[]>{
   };
 
   export async function DeleteFavorite(token:string,symbol:string){
-   
-    const response = await fetch(`${process.env.APIURL}/user/favorites?symbol=${symbol}`, {
+    const apiUrl = process.env.APIURL;
+    const response = await fetch(`${apiUrl}/user/favorites?symbol=${symbol}`, {
       method: 'DELETE',
       headers: {
         Authorization: `Bearer ${token}`
